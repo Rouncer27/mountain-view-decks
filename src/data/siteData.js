8a62c56c20cb16d5b1f77ac6303e1ff7a2ece4b9
Trevor Rounce <trevor@switchbackcreative.ca>
2025-11-25 08:56:46 -0700

    style setup

---
+++ src/data/siteData.js
@@ -0,0 +1,11 @@
+import defaultSrc from "../../public/meta-default-image.jpg";
+
+export const baseData = {
+  title: "Mountain View Sun Decks",
+  organizationName: "Mountain View Sun Decks",
+  description: "Mountain View Sun Decks",
+  siteImg: `https://mountain-view-sun-decks.netlify.app/${defaultSrc.src}`,
+  logoUrl: "",
+  url: "https://mountain-view-sun-decks.netlify.app/",
+  socialProfiles: [],
+};

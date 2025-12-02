6fa57fd0bc4be6b637536346e592e63ca1a748b5
Trevor Rounce <trevor@switchbackcreative.ca>
2025-12-01 18:43:23 -0700

    services post pages

--- src/data/service/queries.js
+++ src/data/service/queries.js
@@ -5,3 +5,48 @@ export const Callouts_Query = `
         }
     }
 `;
+
+export const Image_Content_Query = `
+   	 imageContent: imageContentComponent {
+        title 
+        content
+        image {
+          node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        buttonText
+        buttonLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+    }
+`;
+
+export const Types_Query = `
+   	 types: typesComponent {
+        title 
+        types {
+            title
+            content
+            image {
+                node {
+                    sourceUrl
+                    altText
+                    mediaDetails {
+                        width
+                        height
+                    }
+                }
+            }
+        }                
+    }
+`;
